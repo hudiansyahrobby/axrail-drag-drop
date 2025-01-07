e8f0d88e520d08ca19e6404e279f715442d680f8
hudiansyahrobby <hudiansyahrobby7@gmail.com>
2025-01-08 06:42:07 +0800

    feat: extend Tailwind CSS theme with custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#01959F",
+          50: "#F7FEFF",
+          100: "#4DB5BC",
+        },
+
+        warning: {
+          DEFAULT: "#FA9810",
+          50: "#FFFCF5",
+          100: "#FEEABC",
+        },
+        black: {
+          DEFAULT: "#1D1F20",
+          100: "#E0E0E0",
+          400: "#FAFAFA",
+        },
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
