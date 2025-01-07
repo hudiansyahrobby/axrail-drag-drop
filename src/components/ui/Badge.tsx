3c07c5859a04c58bc7fe50eeb8c84567557bf447
hudiansyahrobby <hudiansyahrobby7@gmail.com>
2025-01-08 06:36:30 +0800

    feat: add Badge component with variant support

---
+++ src/components/ui/Badge.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+
+const badgeVariants = cva(
+  "inline-flex items-center rounded-[4px] border px-2 py-1 text-xs",
+  {
+    variants: {
+      variant: {
+        primary: "border-primary-100 border bg-primary-50 text-primary",
+        warning: "border-warning-100 border bg-warning-50 text-warning",
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+    },
+  }
+);
+
+export interface BadgeProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariants> {}
+
+function Badge({ className, variant, ...props }: BadgeProps) {
+  return (
+    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+  );
+}
+
+export default Badge;
